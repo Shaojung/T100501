b28aa9979060b9f80982060c04f31373c22965a2
Shaojung <abc@abc.com>
2016-10-05 19:33:19 +0800

    getList

--- app/src/main/java/tw/com/pcschool/t100501/data/PersonDAOFileImpl.java
+++ app/src/main/java/tw/com/pcschool/t100501/data/PersonDAOFileImpl.java
@@ -2,6 +2,13 @@ package tw.com.pcschool.t100501.data;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,7 +34,30 @@ public class PersonDAOFileImpl
 
     @Override
     public List<Person> getList() {
-        return null;
+        ArrayList<Person> mylist = new ArrayList();
+
+        char[] buffer = new char[1];
+        FileReader fr = null;
+        StringBuilder sb = new StringBuilder();
+        File file = new File(context.getFilesDir().getAbsolutePath(),
+                "person.txt");
+        try {
+            fr = new FileReader(file);
+            while (fr.read(buffer)!= -1) {
+                sb.append(new String(buffer));
+            }
+            Gson gson = new Gson();
+            mylist = gson.fromJson(sb.toString(), new TypeToken<List<Person>>(){}.getType());
+        }
+        catch (IOException e) { }
+        finally {
+            try {
+                fr.close(); // 關閉檔案
+            }
+            catch (IOException e) { }
+        }
+
+        return mylist;
     }
 
     @Override
